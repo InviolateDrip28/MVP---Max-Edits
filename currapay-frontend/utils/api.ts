fde418343aabbac6e1ab62e74006416b4b2f2f6c
Adaline <90662211+AdalineL@users.noreply.github.com>
2024-09-05 02:37:21 -0700

    updated utils/api.ts

--- currapay-frontend/utils/api.ts
+++ currapay-frontend/utils/api.ts
@@ -1,7 +1,8 @@
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000', 
+  baseURL: process.env.API_BASE_URL || 'http://localhost:4000/trpc', 
   headers: {
     'Content-Type': 'application/json',
   },
