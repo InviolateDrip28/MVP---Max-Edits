81ffea55d5ed1194dc72ec076c136c10be58546d
AdalineL <90662211+AdalineL@users.noreply.github.com>
2024-11-28 21:48:49 -0700

    fixes to index

--- currapay-backend/src/index.ts
+++ currapay-backend/src/index.ts
@@ -1,24 +1,38 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-import cors from 'cors'; 
-import { appRouter } from './trpc/_app';
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { errorHandler } from './utils/errorHandler';
-import { logger } from './utils/logger';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+import { appRouter } from "./trpc/_app";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { errorHandler } from "./utils/errorHandler";
+import { logger } from "./utils/logger";
 
 // Load environment variables
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // Initialize the Express application
 const app = express();
 
+// Get allowed origins
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? process.env.PROD_URL?.split(",") ?? []
+    : [process.env.DEV_URL ?? "http://localhost:3000"];
+
 // CORS setup
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"), false);
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -30,15 +44,18 @@ app.use((req, res, next) => {
 });
 
 // Define a route for the root path
-app.get('/', (req, res) => {
-  res.send('Welcome to the tRPC API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the tRPC API!");
 });
 
 // tRPC setup
-app.use('/trpc', trpcExpress.createExpressMiddleware({
-  router: appRouter,
-  createContext: () => ({}),
-}));
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => ({}),
+  })
+);
 
 // Error handling middleware
 app.use(errorHandler);
