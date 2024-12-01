4c609e514867b82390f46efe9758921077fc03b2
AdalineL <90662211+AdalineL@users.noreply.github.com>
2024-12-01 01:55:59 -0700

    fixed comments

--- currapay-frontend/utils/trpc.ts
+++ currapay-frontend/utils/trpc.ts
@@ -4,7 +4,7 @@ import { AppRouter } from "../../currapay-backend/src/trpc/_app";
 
 function getBaseUrl() {
   // use localhost or production url
-  // const baseUrl = process.env.API_BASE_URL;
+  // const baseUrl = `http://localhost:4000`;
   const baseUrl = `https://currapay-backend-v1-346c542a874a.herokuapp.com`;
 
   return baseUrl;
