219ee610c5ca45e67620e13d4a297b8043939b37
ni-jessica <jessica_ni@brown.edu>
2025-03-07 01:26:36 -0500

    add metadata and icon

--- currapay-frontend/app/layout.tsx
+++ currapay-frontend/app/layout.tsx
@@ -18,6 +18,15 @@ export const metadata: Metadata = {
   title: "CurraPay",
   description:
     "Find the Cheapest Ways to Send Money Abroad and Save Big on Fees with CurraPay",
+  icons: {
+    icon: "/icon.ico",
+  },
+  openGraph: {
+    siteName: "CurraPay",
+    url: "https://currapay.com",
+    title: "Compare International Money Transfer Providers and Save Your Money",
+    description: "Find the Cheapest Ways to Send Money Abroad and Save Big on Fees with CurraPay",
+  }
 };
 
 export default function RootLayout({
