3354d135a13baadd42e1bc43a3412fa2c21f3134
AdalineL <90662211+AdalineL@users.noreply.github.com>
2025-04-06 02:07:55 -0500

    deploy error

--- currapay-frontend/app/profile/dashboard/TransactionsTable.tsx
+++ currapay-frontend/app/profile/dashboard/TransactionsTable.tsx
@@ -10,15 +10,31 @@ enum TransactionStatus {
   PENDING = "pending",
 }
 
-{
-  /* TODO:  add loading, add error handling, fix date time */
-}
+/* TODO:  add loading, add error handling, fix date time */
 export const TransactionsTable = () => {
   const userStore = useUserStore();
   const showTransactions = useRef(false);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const { data, error, isLoading } =
-    trpc.transaction.getTransactionsByUserId.useQuery(userStore.id);
+    trpc.transaction.getTransactionsByUserId.useQuery(
+      userStore.id !== undefined ? userStore.id : -1
+    );
+
+  if (isLoading) {
+    return (
+      <div className="text-center mt-16 font-semibold text-secondary space-y-8 overflow-hidden rounded-xl bg-white border border-secondary/30 shadow p-4 lg:p-6">
+        <h4>Loading your transactions...</h4>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="text-center mt-16 font-semibold text-secondary space-y-8 overflow-hidden rounded-xl bg-white border border-secondary/30 shadow p-4 lg:p-6">
+        <h4>Error fetching transactions. Please try again later.</h4>
+      </div>
+    );
+  }
 
   useEffect(() => {
     if (error) {
@@ -50,21 +66,11 @@ export const TransactionsTable = () => {
           <table className="min-w-full border-collapse border-spacing-y-2 border-spacing-x-2">
             <thead className="hidden border-b border-secondary/30 lg:table-header-group">
               <tr className="w-full font-semibold uppercase">
-                <td className="whitespace-normal py-4 w-3/12">
-                  Date
-                </td>
-                <td className="whitespace-normal py-4 w-3/12">
-                  Provider
-                </td>
-                <td className="whitespace-normal py-4 w-2/12">
-                  Corridor
-                </td>
-                <td className="whitespace-normal py-4 w-3/12">
-                  Amount
-                </td>
-                <td className="whitespace-normal py-4 w-1/12">
-                  Status
-                </td>
+                <td className="whitespace-normal py-4 w-3/12">Date</td>
+                <td className="whitespace-normal py-4 w-3/12">Provider</td>
+                <td className="whitespace-normal py-4 w-2/12">Corridor</td>
+                <td className="whitespace-normal py-4 w-3/12">Amount</td>
+                <td className="whitespace-normal py-4 w-1/12">Status</td>
               </tr>
             </thead>
 
@@ -133,8 +139,7 @@ export const TransactionsTable = () => {
                       {transaction.currencyFrom}
                     </span>
 
-                    {transaction.status ===
-                    TransactionStatus.COMPLETED ? (
+                    {transaction.status === TransactionStatus.COMPLETED ? (
                       <span className="mt-1 ml-auto block w-fit lg:hidden whitespace-nowrap rounded-md bg-green-200 px-2 py-1 text-green-800 font-medium">
                         Completed
                       </span>
@@ -145,8 +150,7 @@ export const TransactionsTable = () => {
                     )}
                   </td>
                   <td className="whitespace-nowrap hidden py-6 lg:table-cell">
-                    {transaction.status ===
-                    TransactionStatus.COMPLETED ? (
+                    {transaction.status === TransactionStatus.COMPLETED ? (
                       <span className="whitespace-nowrap rounded-md bg-green-200 px-2 py-1 text-green-800 font-medium">
                         Completed
                       </span>
@@ -165,8 +169,7 @@ export const TransactionsTable = () => {
         <div className="text-center mt-16 font-semibold text-secondary space-y-8 overflow-hidden rounded-xl bg-white border border-secondary/30 shadow p-4 lg:p-6">
           <h4> No transactions yet.</h4>
           <h4 className="">
-            Start sending money to track your transactions with
-            CurraPay!
+            Start sending money to track your transactions with CurraPay!
           </h4>
         </div>
       )}
