264e3b1b2a318de2fc6fefaa9b6dd506570cc8d4
InviolateDrip28 <max@currapay.com>
2025-04-11 17:08:36 -0400

    Update page.tsx

--- currapay-frontend/app/page.tsx
+++ currapay-frontend/app/page.tsx
@@ -229,7 +229,7 @@ const Homepage = observer(() => {
                   >
                     <path
                       d="M2 29C20.2154 33.6961 38.9915 35.1324 57.6111 37.5555C80.2065 40.496 102.791 43.3231 125.556 44.5555C163.184 46.5927 201.26 45 238.944 45C312.75 45 385.368 30.7371 458.278 20.6666C495.231 15.5627 532.399 11.6429 569.278 6.11109C589.515 3.07551 609.767 2.09927 630.222 1.99998C655.606 1.87676 681.208 1.11809 706.556 2.44442C739.552 4.17096 772.539 6.75565 805.222 11.5C828 14.8064 850.34 20.2233 873 24"
-                      stroke="#545556"
+                      stroke="#9A67FF"
                       strokeWidth="3"
                       strokeLinecap="round"
                       strokeDasharray="1 12"
@@ -238,7 +238,7 @@ const Homepage = observer(() => {
                 </div>
                 <div className="relative grid grid-cols-1 text-center gap-y-8 sm:gap-y-10 lg:gap-y-12 lg:grid-cols-3 lg:gap-x-24 xl:gap-x-36">
                   <div>
-                    <div className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 mx-auto bg-accentDark border-2 border-[#4508bf] rounded-full shadow-xl">
+                    <div className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 mx-auto bg-accentDark border-2 border-[#9A67FF] rounded-full shadow-xl">
                       <h1 className="font-semibold text-white">1</h1>
                     </div>
                     <h2 className="mt-4 sm:mt-6 md:mt-10 lg:h-36 xl:h-28">
@@ -250,7 +250,7 @@ const Homepage = observer(() => {
                     </p>
                   </div>
                   <div>
-                    <div className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 mx-auto bg-accentDark border-2 border-[#4508bf] rounded-full shadow-xl">
+                    <div className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 mx-auto bg-accentDark border-2 border-[#9A67FF] rounded-full shadow-xl">
                       <h1 className="font-semibold text-white">2</h1>
                     </div>
 
@@ -265,7 +265,7 @@ const Homepage = observer(() => {
                     </p>
                   </div>
                   <div>
-                    <div className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 mx-auto bg-accentDark border-2 border-[#4508bf] rounded-full shadow-xl">
+                    <div className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 mx-auto bg-accentDark border-2 border-[#9A67FF] rounded-full shadow-xl">
                       <h1 className="font-semibold text-white">3</h1>
                     </div>
                     <h2 className="mt-4 sm:mt-6 md:mt-10 lg:h-36 xl:h-28">
